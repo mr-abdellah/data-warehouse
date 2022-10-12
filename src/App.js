1a256de4254bea382f2de513b74fb31797bf4407
Abdellah Belkaid <belkaid.abdulah@gmail.com>
2022-10-12 22:10:15 +0100

    Add responsive hero section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./app/Header/Header";
+import Hero from "./app/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
-      <section className="hero">
+      <section>
         <Header />
+        <Hero />
       </section>
     </div>
   );
